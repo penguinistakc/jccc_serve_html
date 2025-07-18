477dc9cdb275544b17825cd4c8c693f599d62e1b
Matthew Copple <mcopple@kcopensource.org>
2025-07-18 18:39:09 -0500

    Removed routeMap, included new getViewURL function to match against pages placed in the views file.

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 "use strict"
 
+const port = 3000;
+const http = require("http");
+const httpStatus = require("http-status-codes");
+const fs = require("fs");
+
+const getViewURL = (url) => {
+    return `views${url}.html`
+}
+
+const app = http.createServer();
+
+app.on("request", (req, res) => {
+    let viewURL = getViewURL(req.url);
+    fs.readFile(viewURL, (error, data) => {
+        if (error) {
+            res.writeHead(httpStatus.StatusCodes.NOT_FOUND);
+            res.write("<h1>FILE NOT FOUND</h1>");
+        } else {
+            res.writeHead(httpStatus.StatusCodes.OK, {
+                "Content-Type": "text/html"
+            });
+            res.write(data);
+        }
+        res.end();
+    })
+    }).listen(port);
+
+console.log(`The server has started and is listening on port number: ${port}`)
